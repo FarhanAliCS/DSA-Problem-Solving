567daa778bbdb8d298fd668ba1f79991acc7f310
FarhanAliCs <farhanoff12@gmail.com>
2026-09-21 20:22:18 +0500

    Little changes in problem 5

---
+++ Day 36 - Bubble Sort/Problem #5 - Count Number of Swap .py
@@ -0,0 +1,14 @@
+arr=[1,4,5,3,1,2]
+
+swap=0
+for i in range(len(arr)):
+    swapped=False
+    for j in range(len(arr)-1-i):
+        if arr[j] > arr[j+1]:
+            arr[j],arr[j+1]=arr[j+1],arr[j]
+            swapped=True
+            swap+=1
+    if not swapped:
+        break
+    
+print(swap)
